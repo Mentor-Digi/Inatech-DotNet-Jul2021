36b7d3a589550f08c522e21b5b8a31b329202dd6
Mentor-Digi <62875230+Mentor-Digi@users.noreply.github.com>
2021-08-03 14:58:43 +0530

    Add files via upload

---
+++ Day 5 - 3rd Aug'21/Programs/Sere.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace FirstConsole
+{
+    class Sere
+    {
+        public void sereData()
+        {
+            FileStream fs = new FileStream("D:\\Inatech\\data.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, "Visit Chennai");
+            fs.Close();
+        }
+        public void deseData()
+        {
+            FileStream fs = new FileStream("D:\\Inatech\\data.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            String str =(String) bf.Deserialize(fs);
+            Console.WriteLine(str);
+            fs.Close();
+        }
+        public static void Main()
+        {
+            Sere sobj = new Sere();
+            //sobj.sereData();
+            sobj.deseData();
+            Console.Read();
+        }
+    }
+}
